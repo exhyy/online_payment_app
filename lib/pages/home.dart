1f891dc02bc3a19fc80872e05e699b70f67d3811
exhyy <starryhyy@gmail.com>
2023-04-30 19:40:09 +0800

    完成home页面结构的基本设计

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:online_payment_app/services/base.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -26,15 +27,136 @@ class _HomeState extends State<Home> {
         backgroundColor: const Color.fromRGBO(242, 242, 242, 1.0),
       ),
       body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(15)),
+          BaseCard(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                HomeButton(
+                  title: '付款',
+                  assetPath: 'assets/icons/cash.png',
+                ),
+                HomeButton(
+                  title: '付款',
+                  assetPath: 'assets/icons/money-bag-e.png',
+                )
+              ],
             ),
-            width: double.infinity,
-            child: const SizedBox(
-              height: 100,
+          ),
+          BaseCard(
+            child: Column(
+              children: const [
+                HomeListTile(
+                  title: '余额',
+                  assetPath: 'assets/icons/yuan.png',
+                  comment: '￥666.66',
+                ),
+                HomeListTile(
+                  title: '银行卡',
+                  assetPath: 'assets/icons/credit.png',
+                ),
+              ],
+            ),
+          ),
+          const BaseCard(
+            child: HomeListTile(
+              title: '信息',
+              assetPath: 'assets/icons/driver-license.png',
+            ),
+          ),
+          BaseCard(
+            child: Column(
+              children: const [
+                HomeListTile(
+                  title: '近期交易',
+                  assetPath: 'assets/icons/invoice.png',
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HomeListTile extends StatelessWidget {
+  final String assetPath;
+  final String title;
+  final String? comment;
+
+  const HomeListTile({
+    super.key,
+    required this.title,
+    required this.assetPath,
+    this.comment,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> trailingChildren = [];
+    if (comment != null) {
+      trailingChildren.add(Text(
+        comment!,
+        style: const TextStyle(
+          fontSize: 26,
+        ),
+      ));
+    }
+    trailingChildren.add(const Icon(
+      Icons.chevron_right,
+      size: 35,
+    ));
+
+    return ListTile(
+      leading: Image.asset(
+        assetPath,
+        width: 40,
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 26,
+        ),
+      ),
+      trailing: SizedBox(
+        width: 160,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: trailingChildren,
+        ),
+      ),
+    );
+  }
+}
+
+class HomeButton extends StatelessWidget {
+  final String assetPath;
+  final String title;
+
+  const HomeButton({
+    super.key,
+    required this.assetPath,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 20,
+      ),
+      child: Column(
+        children: [
+          Image.asset(
+            assetPath,
+            width: 90,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 26,
             ),
           )
         ],
---
+++ lib/services/base.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class BaseCard extends StatelessWidget {
+  final Widget child;
+
+  const BaseCard({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+      ),
+      width: double.infinity,
+      child: child,
+    );
+  }
+}
