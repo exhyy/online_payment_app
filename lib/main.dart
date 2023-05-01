6b3c65c28edddb65ef6d755dfbc2942d104d13e6
exhyy <starryhyy@gmail.com>
2023-05-01 15:23:49 +0800

    登录界面添加了提示信息和页面跳转

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:online_payment_app/pages/home.dart';
 import 'package:online_payment_app/pages/login.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(MaterialApp(
+    builder: FToastBuilder(),
     debugShowCheckedModeBanner: false,
     initialRoute: '/login',
     routes: {
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:online_payment_app/env/env.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:online_payment_app/pages/home.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -14,6 +16,53 @@ class _LoginState extends State<Login> {
   TextEditingController passwordController = TextEditingController();
 
   final dio = Dio();
+  late FToast fToast;
+
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+  }
+
+  _showToast(bool success) {
+    Color color = Colors.greenAccent;
+    Icon icon = const Icon(Icons.check);
+    Text text = const Text('登录成功');
+    if (success == false) {
+      color = Colors.redAccent;
+    }
+    if (success == false) {
+      icon = const Icon(Icons.close);
+    }
+    if (success == false) {
+      text = const Text('手机号码或密码错误');
+    }
+
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: color,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          icon,
+          const SizedBox(
+            width: 12.0,
+          ),
+          text,
+        ],
+      ),
+    );
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.BOTTOM,
+      toastDuration: const Duration(seconds: 2),
+    );
+  }
 
   void configureDio() {
     dio.options.baseUrl = Env.baseUrl;
@@ -21,12 +70,18 @@ class _LoginState extends State<Login> {
     dio.options.receiveTimeout = const Duration(seconds: 3);
   }
 
-  void login() async {
+  Future<void> login() async {
     Response response = await dio.post('/user/authenticate', data: {
       'mobileNumber': mobileNumberController.text,
       'password': passwordController.text
     });
-    print(response.data.toString());
+    if (response.data.toString() == 'pass' && mounted) {
+      _showToast(true);
+      // 跳转到Home并且不允许返回
+      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) => const Home()), (route) => false);
+    } else {
+      _showToast(false);
+    }
   }
 
   @override
@@ -38,7 +93,7 @@ class _LoginState extends State<Login> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Center(
+          const Center(
             child: Text(
               '在线支付APP',
               style: TextStyle(
@@ -47,6 +102,7 @@ class _LoginState extends State<Login> {
               ),
             ),
           ),
+          const SizedBox(height: 30),
           TextField(
             controller: mobileNumberController,
             cursorHeight: 30,
@@ -61,14 +117,15 @@ class _LoginState extends State<Login> {
               labelText: '密码',
             ),
           ),
-          MaterialButton(
-            child: Text(
+          const SizedBox(height: 50),
+          ElevatedButton(
+            onPressed: login,
+            child: const Text(
               '登录',
               style: TextStyle(
                 fontSize: 20,
               ),
             ),
-            onPressed: login,
           ),
         ],
       ),
